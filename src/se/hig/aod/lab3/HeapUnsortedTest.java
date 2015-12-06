6f6fb38057e11a2631f92282099c2f876a6b65f8
MiranBatti <miran.batti@gmail.com>
2015-12-06 21:41:56 +0100

    Vad göra? :S

---
+++ src/se/hig/aod/lab3/HeapUnsortedTest.java
@@ -0,0 +1,49 @@
+package se.hig.aod.lab3;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;;
+
+
+public class HeapUnsortedTest {
+	public static void main(String[] args) throws DuplicateItemException, EmptyQueueException {
+		HeapPriorityQueue<Integer> pq = new HeapPriorityQueue<Integer>(640000);
+		ArrayList<Integer> biggerL = null;
+		ArrayList<Integer> smallerL = null;
+		Reader r = new Reader();
+		
+		try {
+			biggerL = r.loadList("C:/Users/Miran/git/AoDLab3/src/resources/data_640000.txt", 640000);
+			smallerL = r.loadList("C:/Users/Miran/git/AoDLab3/src/resources/data_6400.txt", 6400);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		long t1 = System.currentTimeMillis();
+		
+		for (int i = 0; i < biggerL.size(); i++) {
+			pq.enqueue(biggerL.get(i));
+		}
+		
+		for (int i = 0; i < smallerL.size(); i++) {
+			pq.enqueue(smallerL.get(i));
+		}
+		
+		for (int i = 0; i < smallerL.size(); i++) {
+			System.out.println(pq.dequeue());
+		}
+		
+		for (int i = 0; i < biggerL.size(); i++) {
+			System.out.println(pq.dequeue());
+		}
+		
+		long exetime = System.currentTimeMillis() - t1;
+		
+		System.out.println(exetime);
+	}
+}
---
+++ src/se/hig/aod/lab3/Reader.java
@@ -0,0 +1,28 @@
+package se.hig.aod.lab3;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Reader {
+
+	public Reader() {
+		
+	}
+	
+	public ArrayList<Integer> loadList(String path, int size) throws FileNotFoundException, IOException {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int cnt = 0;
+		BufferedReader in = new BufferedReader(new FileReader(path));
+		String l;
+		while ((l = in.readLine()) != null && cnt < size) {
+			list.add(Integer.parseInt(l));
+			cnt++;
+		}
+		in.close();
+		return list;
+	}
+
+}
